b54255a27689f2805719c4ca8efe267716f8e45c
Patryk 'PsichiX' Budzynski <psichix@gmail.com>
2020-02-02 10:14:18 +0100

    snapshot

--- ggj20-game/src/systems/virus_beat.rs
+++ ggj20-game/src/systems/virus_beat.rs
@@ -1,13 +1,16 @@
 #![allow(clippy::type_complexity)]
 
 use crate::{
-    components::{ui_element::UiElement, VirusTag},
+    components::{ui_element::*, VirusTag},
     resources::beat::Beat,
 };
 use oxygengine::prelude::*;
 
 #[derive(Debug, Default)]
-pub struct VirusBeatSystem;
+pub struct VirusBeatSystem {
+    // NOTE: don't put that here.
+    beat: usize,
+}
 
 impl<'s> System<'s> for VirusBeatSystem {
     type SystemData = (
@@ -18,6 +21,16 @@ impl<'s> System<'s> for VirusBeatSystem {
 
     fn run(&mut self, (beat, viruses, mut ui_elements): Self::SystemData) {
         for (virus, ui_element) in (&viruses, &mut ui_elements).join() {
+            if beat.pulse() {
+                // NOTE: don't make that like that
+                // self.beat = (self.beat << (self.beat + self.beat * 2)) % 11;
+                self.beat = (self.beat + 1) % 22;
+                if let UiElementType::Image(image) = &mut ui_element.element_type {
+                    image.image =
+                        UiImagePath::Single(format!("images/{}_virus.png", self.beat / 2));
+                }
+                ui_element.rebuild();
+            }
             if beat.is_sync_with_beat(0.1) {
                 ui_element.scale = 1.5.into();
             } else {
